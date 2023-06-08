5bd895d6ee1ff14f00c3b6fb098ce204ee74d390
trace <100058383+7R4C3@users.noreply.github.com>
2023-06-08 09:51:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 # Installation script URL
 install_script_url="https://github.com/7R4C3/D3m0n1z3dShell/raw/main/demonizedshell.sh"
 
-# Download and execute the installation script
-curl -sSL $install_script_url | bash
+# Download the installation script
+curl -sSL $install_script_url | sudo bash
+
